175d7573ae4d11fc78c39ff9393f398cb0cbba3d
Mathias San Miguel <enlovson@amazon.com>
2024-03-03 21:13:04 +0000

    Added some lazy/optional colorschemes

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -35,7 +35,7 @@ return {
 
 	{
 		"folke/tokyonight.nvim",
-		event = "VeryLazy",
+		lazy = true,
 		opts = {
 			dim_inactive = true,
 			transparent = true,
@@ -46,4 +46,22 @@ return {
 			},
 		},
 	},
+
+	{
+		'nyngwang/nvimgelion',
+		lazy = true,
+		config = function ()
+			vim.api.nvim_create_autocmd({ 'ColorScheme', 'FileType' }, {
+				callback = function ()
+					vim.cmd([[
+						hi IndentBlanklineChar gui=nocombine guifg=#444C55
+						hi IndentBlanklineSpaceChar gui=nocombine guifg=#444C55
+						hi IndentBlanklineContextChar gui=nocombine guifg=#FB5E2A
+						hi IndentBlanklineContextStart gui=underline guisp=#FB5E2A
+					]])
+				end,
+			})
+		end
+	}
+
 }
