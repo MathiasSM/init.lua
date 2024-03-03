8d221c7dadb1f86e469925532b03487fc48feb61
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-03 19:53:54 +0000

    Added catpuccin config

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -3,17 +3,47 @@ return {
 		"catppuccin/nvim",
 		lazy = false,
 		priority = 1000,
-		config = function() vim.cmd([[colorscheme catppuccin]]) end,
+		config = function()
+			require('catppuccin').setup({
+				styles = {
+					conditionals = { }, -- Defaults to italics
+				},
+				integrations = {
+					harpoon=true,
+					headlines=true,
+					markdown=true,
+					mason=true,
+					neotree=true,
+					neotest=true,
+					treesitter_context = false,
+					lsp_trouble = true,
+					which_key = true,
+					native_lsp = {
+						enabled = true,
+						underlines = {
+							errors = { "underline" },
+							warnings = { "underline" },
+							hints = { },
+							information = { },
+						},
+					},
+				}
+			})
+			vim.cmd([[colorscheme catppuccin]])
+		end,
 	},
 
 	{
 		"folke/tokyonight.nvim",
 		event = "VeryLazy",
 		opts = {
-			styles = { comments = { italic = true } },
 			dim_inactive = true,
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+				comments = { italic = true }
+			},
 		},
 	},
-
-	{ "ellisonleao/gruvbox.nvim" },
 }
