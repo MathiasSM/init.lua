a8e808598560ab730cfd4e5dd36e82d13e28e64b
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-13 13:44:52 +0100

    Added better typescript support

--- lua/plugins/lsp/other.lua
+++ lua/plugins/lsp/other.lua
@@ -6,6 +6,16 @@ return {
 
 	{
 		"mfussenegger/nvim-jdtls",
-		ft = "java"
-	}
+		ft = "java",
+	},
+
+	{
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		opts = {
+			settings = {
+				expose_as_code_action = "all",
+			},
+		},
+	},
 }
