8d1a62017f36e8f19bd943979dd3b8139cf6dafe
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-13 13:47:54 +0100

    Lazy-load typescript only on... ts (and js)

--- lua/plugins/lsp/other.lua
+++ lua/plugins/lsp/other.lua
@@ -12,6 +12,7 @@ return {
 	{
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+		ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
 		opts = {
 			settings = {
 				expose_as_code_action = "all",
