f9a8a9c1c69b03482b2024f4b6bee3e6a7da63b7
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-13 12:55:29 +0100

    Added DAP plugins (persistnet-breakpoints, goto-breakpoints)

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -34,6 +34,27 @@ return {
 		config = true,
 	},
 
+	{
+		"Weissle/persistent-breakpoints.nvim",
+		cmd = { "PBToggleBreakpoint", "PBSetConditionalBreakpoint", "PBClearAllBreakpoints" },
+		config = function()
+			require("persistent-breakpoints").setup({
+				load_breakpoints_event = { "BufReadPost" },
+			})
+		end,
+	},
+
+	{
+		"ofirgall/goto-breakpoints.nvim",
+		dependencies = {"mfussenegger/nvim-dap"},
+		keys = { "]b", "[b", "]S" },
+		config = function()
+			vim.keymap.set("n", "]d", require("goto-breakpoints").next, {desc = "[Debug] Prev breakpoint"})
+			vim.keymap.set("n", "[d", require("goto-breakpoints").prev, {desc = "[Debug] Next breakpoint"})
+			vim.keymap.set("n", "]S", require("goto-breakpoints").stopped, {desc = "[Debug] Current stopped line"})
+		end,
+	},
+
 	-- Configured alongside other LSPs
 	"mfussenegger/nvim-jdtls",
 	"mrcjkb/haskell-tools.nvim",
