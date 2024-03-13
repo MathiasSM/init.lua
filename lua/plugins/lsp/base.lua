04815b3c65ce6c11f458af7c919b5a8e6455c32e
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-13 12:46:26 +0100

    Added some lsp configs (limit custom-elements to html!)

--- lua/plugins/lsp/base.lua
+++ lua/plugins/lsp/base.lua
@@ -1,20 +1,60 @@
 local function get_handlers()
+	local completion_capabilities = require("cmp_nvim_lsp").default_capabilities()
 	return {
 		-- [1]: Default handler
 		function(server_name)
 			require("lspconfig")[server_name].setup({
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				capabilities = completion_capabilities,
 			})
 		end,
 		-- [*] Specific handlers
-		["jsonls"] = function()
-			require("lspconfig").jsonls.setup({
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
+		["jsonls"] = function(server_name)
+			require("lspconfig")[server_name].setup({
+				capabilities = completion_capabilities,
 				settings = {
 					json = {
 						schemas = require("schemastore").json.schemas(),
 						validate = { enable = true },
 					},
+				},
+			})
+		end,
+		["tsserver"] = function(server_name)
+			local capabilities = vim.deepcopy(completion_capabilities)
+			-- Disable tsserver as formatter
+			completion_capabilities.documentFormattingProvider = false
+			completion_capabilities.documentRangeFormattingProvider = false
+			require("lspconfig")[server_name].setup({
+				capabilities = capabilities,
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			})
+		end,
+		["bashls"] = function(server_name)
+			require("lspconfig")[server_name].setup({
+				capabilities = completion_capabilities,
+				filetypes = { "sh", "bash", "zsh" },
+			})
+		end,
+		["custom_elements_ls"] = function(server_name)
+			require("lspconfig")[server_name].setup({
+				capabilities = completion_capabilities,
+				settings = {
+					json = {
+						schemas = require("schemastore").json.schemas(),
+						validate = { enable = true },
+					},
+				},
+				filetypes = {
+					"html",
+					"typescript",
+					"javascript",
+					"typescriptreact",
+					"javascriptreact",
 				},
 			})
 		end,
@@ -26,7 +66,13 @@ return {
 		"neovim/nvim-lspconfig",
 		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 		dependencies = {
-			"folke/neodev.nvim", -- Neodev must run before lspconfig
+			-- Neodev must run before lspconfig
+			"folke/neodev.nvim",
+			-- TODO. Want it to run when LSP runs
+			{
+				"j-hui/fidget.nvim",
+				opts = {},
+			},
 			"williamboman/mason-lspconfig.nvim",
 		},
 		config = function()
