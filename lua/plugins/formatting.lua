16e409c411cb88d7a59659844c46f29321b3cd56
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-17 14:28:45 +0100

    Made format mapping lsp-first

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,5 +1,8 @@
---- Formatters configuration
--- These should be installed (via Mason or otherwise)
+--- Formatting configuration
+--
+-- These formatters will be used if LSP provides no formatting capability
+-- 
+-- They should be installed first (via Mason or otherwise)
 --
 -- @module formatters
 
@@ -8,33 +11,40 @@
 -- For each ft, formatters are applied in order.
 -- The `*` applies to all filetypes.
 local get_formatters_by_ft = function()
-	local js = require("formatter.filetypes.javascript")
-	local jsx = require("formatter.filetypes.javascriptreact")
 	local lua = require("formatter.filetypes.lua")
 	local sh = require("formatter.filetypes.sh")
 	local sql = require("formatter.filetypes.sql")
-	local ts = require("formatter.filetypes.typescript")
-	local tsx = require("formatter.filetypes.typescriptreact")
 	local any = require("formatter.filetypes.any")
 	return {
-		javascript = { js.eslint_d },
-		javascriptreact = { jsx.eslint_d },
+		-- javascript: eslint-lsp
+		-- javascriptreact: eslint-lsp
 		lua = { lua.stylua },
 		sh = { sh.shfmt },
 		sql = { sql.pgformat, sql.sqlfluff },
-		typescript = { ts.eslint_d },
-		typescriptreact = { tsx.eslint_d },
+		-- typescript: eslint-lsp
+		-- typescriptreact: eslint-lsp
 		["*"] = { any.remove_trailing_whitespace },
 	}
 end
+
+local Utils = require("utils")
 
 return {
 	{
 		"mhartington/formatter.nvim",
 		keys = {
-			{ "<leader>pp", ":FormatWrite<CR>", desc = "[Formatter] Run" },
+			{
+				"<leader>p",
+				function()
+					if Utils.lsp_has_formatting() then
+						vim.lsp.buf.format()
+					else
+						vim.cmd("FormatWrite")
+					end
+				end,
+				desc = "[Format] Run",
+			},
 		},
-		cmd = { "Format", "FormatWrite" },
 		config = function()
 			require("formatter").setup({
 				logging = true,
