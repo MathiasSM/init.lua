30880a11a26e9732d5cc19c6f031f9bb308d01fe
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-17 20:33:13 +0100

    Add telescope mappings to open in trouble

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -32,7 +32,18 @@ return {
 				desc = "[Trouble] Location list",
 			},
 		},
-		config = function() end,
+		config = function()
+			local telescope = require("telescope")
+			local trouble = require("trouble.providers.telescope")
+			telescope.setup({
+				defaults = {
+					mappings = {
+						i = { ["<c-t>"] = trouble.open_with_trouble },
+						n = { ["<c-t>"] = trouble.open_with_trouble },
+					},
+				},
+			})
+		end,
 	},
 
 	{
