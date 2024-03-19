cd033d13eac11efb5d1d48b9cec2ab8f1dbdc4ab
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-19 17:47:07 +0100

    Made LSP lazy and opt-in via mapping, reorganized lsp

---
+++ lua/plugins/lsp-lang-haskell.lua
@@ -0,0 +1,57 @@
+return {
+	{
+		"mrcjkb/haskell-tools.nvim",
+		version = "^3", -- Recommended
+		ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+		config = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = { "haskell", "lhaskell", "cabal", "cabalproject" },
+				desc = "Attach haskell-language-server on haskell files",
+				group = vim.api.nvim_create_augroup("UserLspConfigHaskell", {}),
+				callback = function()
+					require("telescope").load_extension("ht")
+					local ht = require("haskell-tools")
+					local bufnr = vim.api.nvim_get_current_buf()
+					local keyOpts = function(desc)
+						return {
+							noremap = true,
+							silent = true,
+							buffer = bufnr,
+							desc = desc,
+						}
+					end
+					vim.keymap.set(
+						"n",
+						"<space>hs",
+						ht.hoogle.hoogle_signature,
+						keyOpts("[LSP] Haskell: Show hoogle signature")
+					)
+					vim.keymap.set(
+						"n",
+						"<space>he",
+						ht.lsp.buf_eval_all,
+						keyOpts("[LSP] Haskell: Evaluate all")
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>hp",
+						ht.repl.toggle,
+						keyOpts("[LSP] Haskell: Toggle REPL for package")
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>hr",
+						function() ht.repl.toggle(vim.api.nvim_buf_get_name(0)) end,
+						keyOpts("[LSP] Haskell: Toggle REPL for buffer")
+					)
+					vim.keymap.set(
+						"n",
+						"<leader>hR",
+						ht.repl.quit,
+						keyOpts("[LSP] Haskell: Quit REPL")
+					)
+				end,
+			})
+		end,
+	},
+}
