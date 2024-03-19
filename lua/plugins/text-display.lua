77fb1dae463d01fc7f9dd6d5bcc4415cb3bf4c20
Mathias San Miguel <mathiassanmiguel@gmail.com>
2024-03-19 19:40:33 +0100

    Remove multiline TODOs

--- lua/plugins/text-display.lua
+++ lua/plugins/text-display.lua
@@ -51,7 +51,10 @@ return {
 		event = "VeryLazy",
 		opts = {
 			signs = true, -- show icons in the signs column
-			sign_priority = 8, -- sign priority
+			sign_priority = 8, -- sign priority 
+			highlight = {
+				multiline = false,
+			},
 			keywords = {
 				FIX = {
 					icon = " ",
